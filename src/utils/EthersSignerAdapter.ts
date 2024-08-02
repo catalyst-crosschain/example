6473f6cb0081cbd7ee9da57721eeb739e23e3bb0
vivek pal <31vivekpal@gmail.com>
2024-08-02 19:06:09 +0000

    fix: signer

--- src/utils/EthersSignerAdapter.ts
+++ src/utils/EthersSignerAdapter.ts
@@ -1,5 +1,5 @@
 import { Signer as EthersSigner } from "ethers";
-import { Signer, SignedTx, UnsignedTransaction, ChainName } from "@wormhole-foundation/sdk";
+import { Signer, SignedTx, UnsignedTransaction } from "@wormhole-foundation/sdk";
 
 export class EthersSignerAdapter implements Signer {
   constructor(private ethersSigner: EthersSigner, private chainName: ChainName) {}
